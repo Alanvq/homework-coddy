1765cfe4d73c4c2c20afd5ee68c343493a049682
Alanvq <163431944+Alanvq@users.noreply.github.com>
2024-08-24 13:21:05 +0600

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,13 @@
+# input table
+
+z = input("Введите имя: ")
+if z == "Темур-Малик":
+   print("access is allowed")
+elif z == "Алан":
+   print("access is allowed")
+elif z == "Саян":
+   print("access is allowed")
+elif z == "Чынгыз":
+   print("access is allowed")
+else:
+   print("no access")
